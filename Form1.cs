4609e7a6e8c4b6b37c11d2f20dcadd3391d7bc25
Hale Ertem <ertemhale04@gmail.com>
2025-07-23 17:44:36 +0300

    Add project files.

---
+++ Form1.cs
@@ -0,0 +1,100 @@
+﻿//using System;
+//using System.Collections.Generic;
+//using System.ComponentModel;
+//using System.Data;
+//using System.Drawing;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
+//using System.Windows.Forms;
+
+//namespace barkod_reader
+//{
+//    public partial class Form1 : Form
+//    {
+//        public Form1()
+//        {
+//            InitializeComponent();
+//        }
+
+//        private void Form1_Load(object sender, EventArgs e)
+//        {
+
+//        }
+
+//        private void button1_Click(object sender, EventArgs e)
+//        {
+
+//        }
+//    }
+//}
+
+using System;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Drawing.Printing;
+using System.Linq;
+using System.Windows.Forms;
+using ZXing;
+using ZXing.Common;
+
+namespace barkod_reader
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private string GenerateRandomBarcode(int length)
+        {
+            const string chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+            Random random = new Random();
+            return new string(Enumerable.Repeat(chars, length)
+                .Select(s => s[random.Next(s.Length)]).ToArray());
+        }
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string barkodVerisi = GenerateRandomBarcode(10);
+            string dosyaYolu = "barkod.png";
+
+            // Barkodu oluştur ve dosyaya kaydet
+            var yazar = new BarcodeWriter
+            {
+                Format = BarcodeFormat.CODE_128,
+                Options = new EncodingOptions { Width = 300, Height = 100 },
+                Renderer = new ZXing.Rendering.BitmapRenderer()
+            };
+
+            using (var bitmap = yazar.Write(barkodVerisi))
+            {
+                bitmap.Save(dosyaYolu, ImageFormat.Png);
+            }
+
+            // Barkod görselini yazdır
+            Image image = Image.FromFile(dosyaYolu);
+            PrintDocument pd = new PrintDocument();
+
+            // Yazıcı adını belirtmek istersen buraya yaz:
+            // pd.PrinterSettings.PrinterName = "XP-80";
+
+            pd.PrintPage += (s, ev) =>
+            {
+                ev.Graphics.DrawImage(image, new PointF(0, 0));
+            };
+
+            pd.Print();
+
+            MessageBox.Show("Barkod yazdırıldı.");
+        }
+    }
+}
+
---
+++ Program.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace barkod_reader
+{
+    internal static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+        }
+    }
+}
+
+//using System;
+//using System.Drawing;
+//using System.Drawing.Printing;
+//using System.IO;
+//using System.Text;
+
+//class Program
+//{
+//    static void Main()
+//    {
+//        // 1. Yazıcının sistemdeki adını yaz
+//        string printerName = "Xprinter XP-490B";
+
+//        // 2. Rastgele barkod verisi üret
+//        string barcodeData = GenerateRandomCode(8);
+
+//        // 3. TSPL komutlarını hazırla
+//        string tspl = $@"
+//SIZE 70 mm, 50 mm
+//GAP 3 mm, 0 mm
+//DENSITY 8
+//CLS
+//CODE128 50,50,{barcodeData},100,1
+//TEXT 50,160,""3"",0,1,1,""{barcodeData}""
+//PRINT 1
+//";
+
+//        // 4. Yazıcıya gönder
+//        SendRawStringToPrinter(printerName, tspl);
+//        Console.WriteLine($"Yazdırıldı: {barcodeData}");
+//    }
+
+//    static string GenerateRandomCode(int length)
+//    {
+//        const string chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+//        var random = new Random();
+//        var result = new char[length];
+//        for (int i = 0; i < length; i++)
+//            result[i] = chars[random.Next(chars.Length)];
+//        return new string(result);
+//    }
+
+//    static void SendRawStringToPrinter(string printerName, string data)
+//    {
+//        var printerSettings = new PrinterSettings { PrinterName = printerName };
+
+//        if (!printerSettings.IsValid)
+//        {
+//            Console.WriteLine($"Yazıcı bulunamadı: {printerName}");
+//            return;
+//        }
+
+//        var doc = new PrintDocument
+//        {
+//            PrinterSettings = printerSettings,
+//            DocumentName = "XP-490B Barkod",
+//        };
+
+//        doc.PrintPage += (sender, e) =>
+//        {
+//            // Yazıcıya byte olarak TSPL komutlarını gönder
+//            var bytes = Encoding.ASCII.GetBytes(data);
+//            e.Graphics.DrawString("", new Font("Arial", 1), Brushes.Black, 0, 0); // boş çizim, zorunlu
+//            e.Graphics.Dispose(); // çizim tamamlandı
+//        };
+
+//        doc.Print(); // yazdırmayı tetikle
+//    }
+//}
+
